d222ed4bf3d3afea41eea90431388c0273147c7d
Divakar Gaur <divakargaur270@gmail.com>
2025-02-05 10:21:30 +0530

    method_shorthand

--- js/06/function.js
+++ js/06/function.js
@@ -12,3 +12,20 @@ function printTable(num) {
     } return arr
 }
 console.log(printTable(10))
+
+// var does not have block scope
+// lexical scope -> nested function
+// hoisting -> variables can be used before declaration (var)
+// higher order function -> function as an argument or return
+
+// methods -> function as a property of an object
+const calculator = {
+    add: function (a, b) {
+        return a + b;
+    },
+    sub (a, b) { // method shorthand
+        return a - b;
+    }
+}
+
+// internally (almost) everything is an object
